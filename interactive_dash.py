3dd0b5d27266e53c61ff3cac5234ab2dc9389740
jparep <parepson@gmail.com>
2025-01-15 22:35:05 -0700

    Load data

--- interactive_dash.py
+++ interactive_dash.py
@@ -1,3 +1,10 @@
 from dash import Dash, dcc, Input, Output
 import plotly.express as px
 import dash_bootstrap_components as dbc
+import pandas as pd
+import numpy as np
+
+# Load data
+train_df = pd.read_csv('/data/train.csv')
+test_df  = pd.read_csv('/data/test.csv')
+

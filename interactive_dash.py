dd3cb0f42b0879ce12c8fac0e229eb5f6cce073d
jparep <parepson@gmail.com>
2025-01-15 22:36:36 -0700

    Initalize Dashh app

--- interactive_dash.py
+++ interactive_dash.py
@@ -8,3 +8,5 @@ import numpy as np
 train_df = pd.read_csv('/data/train.csv')
 test_df  = pd.read_csv('/data/test.csv')
 
+# Initalize Dash app with a modern Bootstrap theme
+app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
